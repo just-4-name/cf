6156e72bb450aa54e3b2a27811fd1e7cc1c19532
just-4-name <leonid.tara@gmail.com>
2024-12-01 01:59:53 +0300

    add long double define

--- main.cpp
+++ main.cpp
@@ -11,14 +11,18 @@
 #include <unordered_map>
 #include <unordered_set>
 #include <vector>
+#include <iomanip>
 
 using std::cin;
 using std::cout;
 using std::pair;
 using std::set;
 using std::vector;
+using std::map;
+using std::string;
 
 #define int long long
+#define ld long double
 #define vi vector<int>
 #define vvi vector<vector<int>>
 #define omk cout << "YES\n"
@@ -35,58 +39,26 @@ using std::vector;
 #define pii std::pair<int, int>
 #define NL cout << '\n'
 
+
+
+static const int MOD = 998244353;
+
 void OptimizeIO() {
   std::ios_base::sync_with_stdio(false);
   cin.tie(nullptr);
   cout.tie(nullptr);
 }
 
-int Getk(int n, int pos) { return (bool((1 << pos) & n)); }
+
 
 void Solve() {
-  int n, m;
-  cin >> n >> m;
-  vector<pair<int, pii>> a(m); // r,l,sum
-  for (int i = 0; i < m; ++i) {
-    int l, r, s;
-    cin >> l >> r >> s;
-    a[i] = {r - 1, {l - 1, s}};
-  }
-  int M = 10;
-  ssort(a);
-  int curr = 0;
-  int next = -1;
-  vector<vector<int>> dp(n, vector<int>((1 << M), 1));
-  for (int i = 0; i < (1 << M); ++i) {
-    vector<int> cur(10);
-    for (int k = 0; k < 10; ++k) cur[k] = Getk(i, k);
-    int ind = curr;
-    vector<int> pref(10);
-    pref[0] = cur[0];
-    for (int i = 1; i < 10; ++i) pref[i] = pref[i- 1]+cur[i];
-    while (ind < m) {
-      int r = a[ind].first, l  = a[ind].second.first, s = a[ind].second.second;
-      if (r >= 10) {
-        next = r;
-        break;
-      }
-      int ss = pref[r];
-      if (l > 0) ss -= pref[l - 1];
-      if (s != ss) {
-        dp[9][i] = 0;
-      }
-    }
-    for (int pos = 9;)
 
-    vector<int> cur =
-  }
 }
 
 int32_t main() {
   OptimizeIO();
-
   int t = 1;
-  // cin >> t;
+  //cin >> t;
   while (t--)
     Solve();
 }
